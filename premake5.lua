1839e003c2c017b52e52f5b7bd54902414a05817
Dennis Stacer <dstacer@gmail.com>
2020-12-28 21:58:16 -0600

    Resolved conflicts

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 project "imgui"
    kind "StaticLib"
    language "C++"
@@ -31,40 +30,9 @@ project "imgui"
 		symbols "on"
 
    filter "configurations:Release"
-		runtime "Release"
-=======
-project "imgui"
-   kind "StaticLib"
-   language "C++"
-   targetdir ("bin/" .. outputdir)
-   objdir ("Intermediate/" .. outputdir)
-   defines { "IMGUI_IMPL_OPENGL_LOADER_GLAD" }
-
-   files 
-   { 
-		"imconfig.h", 
-		"imgui.h",
-        "imgui.cpp",
-        "imgui_draw.cpp", 
-		"imgui_internal.h",
-        "imgui_widgets.cpp",
-        "imgui_tables.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h",
-        "imgui_demo.cpp"
-   }
-
-   filter "system:windows"
-      staticruntime "On"
-      cppdialect "C++17"
-      systemversion "latest"
-   
-   filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
+        runtime "Release"
+        optimize "on"
+        
    filter "configurations:Release"
 		runtime "Release"
->>>>>>> e79de99997109a67c0c9a5c1e6542c225417a4b9
 		optimize "on"
